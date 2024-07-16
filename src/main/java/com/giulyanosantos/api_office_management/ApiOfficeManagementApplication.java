42bfeb1397fb6e5501d09fabd0723cd730eb8d25
Giulyano Santos <giulyanofelipesantos@gmail.com>
2024-07-15 23:23:38 -0300

    Modificações

---
+++ src/main/java/com/giulyanosantos/api_office_management/ApiOfficeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.giulyanosantos.api_office_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOfficeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiOfficeManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giulyanosantos/api_office_management/ApiOfficeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giulyanosantos.api_office_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOfficeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
